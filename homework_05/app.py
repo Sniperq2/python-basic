db8c503d4e128ed2a56a9d35ce2f08785428e6b2
Sniperq2 <ogldelphi@mail.ru>
2024-03-10 14:56:53 +0300

    Add base Flask application

---
+++ homework_05/app.py
@@ -0,0 +1,23 @@
+"""
+Домашнее задание №5
+Первое веб-приложение
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+"""
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
