dbc8c14c13cdb21475c7fec63d3e4eb21da4100c
Sniperq2 <ogldelphi@mail.ru>
2024-05-22 11:07:35 +0300

    second step

--- homework_09/simpleblog/claims/admin.py
+++ homework_09/simpleblog/claims/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from claims.models import Claim
+from .models import Claim
 
 admin.site.register(Claim)
--- homework_09/simpleblog/claims/models.py
+++ homework_09/simpleblog/claims/models.py
@@ -4,33 +4,15 @@ from django.urls import reverse
 from django.utils import timezone
 
 
-class Claim(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    moderation_till = models.DateTimeField(blank=True, null=True)
-    status = models.ForeignKey('ClaimStatus', on_delete=models.PROTECT)
-    type = models.ForeignKey('ClaimType', on_delete=models.PROTECT)
-    image = models.ForeignKey('Images', on_delete=models.PROTECT)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def get_absolute_url(self):
-        return reverse('claims', kwargs={})
-
-    def __str__(self):
-        return self.title
-
 class ClaimStatus(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+
 class ClaimType(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
+
 
 class Images(models.Model):
     id = models.AutoField(primary_key=True)
@@ -46,3 +28,24 @@ class Images(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Claim(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    moderation_till = models.DateTimeField(blank=True, null=True)
+    status = models.ForeignKey('ClaimStatus', on_delete=models.PROTECT)
+    image = models.ForeignKey('Images', on_delete=models.PROTECT)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('claims', kwargs={})
+
+    def __str__(self):
+        return self.title
+
