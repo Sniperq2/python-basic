541e657e2c84d14e1dd44af1d1e984495116ad64
Sniperq2 <ogldelphi@mail.ru>
2024-05-23 21:21:05 +0300

    step six fix claims url

--- homework_09/simpleblog/claims/urls.py
+++ homework_09/simpleblog/claims/urls.py
@@ -4,7 +4,7 @@ from .views import ShowProposal, profile_view, authView, home, add_claim, about,
 
 urlpatterns = [
     path("", home, name="home"),
-    path('<int:claim_pk>/', ShowProposal.as_view(), name='claim'),
+    path('claims/<int:claim_pk>/', ShowProposal.as_view(), name='claim'),
     path("create/", add_claim, name="add_claim"),
     path("profile/", profile_view, name="profile"),
     path('signup/', authView, name="authView"),
